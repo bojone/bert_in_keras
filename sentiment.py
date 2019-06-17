239e206963e09ff577d1ef2d9ee7e971c2d0f6b7
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-06-17 16:50:11 +0800

    Create sentiment.py

---
+++ sentiment.py
@@ -0,0 +1,126 @@
+#! -*- coding:utf-8 -*-
+
+import json
+import numpy as np
+import pandas as pd
+from random import choice
+from keras_bert import load_trained_model_from_checkpoint, Tokenizer
+import re, os
+import codecs
+
+
+maxlen = 100
+
+
+config_path = '../bert/chinese_L-12_H-768_A-12/bert_config.json'
+checkpoint_path = '../bert/chinese_L-12_H-768_A-12/bert_model.ckpt'
+dict_path = '../bert/chinese_L-12_H-768_A-12/vocab.txt'
+
+
+token_dict = {}
+with codecs.open(dict_path, 'r', 'utf8') as reader:
+    for line in reader:
+        token = line.strip()
+        token_dict[token] = len(token_dict)
+
+
+tokenizer = Tokenizer(token_dict)
+
+
+
+import pandas as pd
+
+neg = pd.read_excel('neg.xls', header=None)
+pos = pd.read_excel('pos.xls', header=None)
+
+data = []
+
+for d in neg[0]:
+    data.append((d, 0))
+
+for d in pos[0]:
+    data.append((d, 1))
+
+
+# 按照9:1的比例划分训练集和验证集
+random_order = range(len(data))
+np.random.shuffle(random_order)
+train_data = [data[j] for i, j in enumerate(random_order) if i % 10 != 0]
+valid_data = [data[j] for i, j in enumerate(random_order) if i % 10 == 0]
+
+
+def seq_padding(X, padding=0):
+    L = [len(x) for x in X]
+    ML = max(L)
+    return np.array([
+        np.concatenate([x, [padding] * (ML - len(x))]) if len(x) < ML else x for x in X
+    ])
+
+
+class data_generator:
+    def __init__(self, data, batch_size=32):
+        self.data = data
+        self.batch_size = batch_size
+        self.steps = len(self.data) // self.batch_size
+        if len(self.data) % self.batch_size != 0:
+            self.steps += 1
+    def __len__(self):
+        return self.steps
+    def __iter__(self):
+        while True:
+            idxs = range(len(self.data))
+            np.random.shuffle(idxs)
+            X1, X2, Y = [], [], []
+            for i in idxs:
+                d = self.data[i]
+                text = d[0][:maxlen]
+                x1, x2 = tokenizer.encode(first=text)
+                y = d[1]
+                X1.append(x1)
+                X2.append(x2)
+                Y.append([y])
+                if len(X1) == self.batch_size or i == idxs[-1]:
+                    X1 = seq_padding(X1)
+                    X2 = seq_padding(X2)
+                    Y = seq_padding(Y)
+                    yield [X1, X2], Y
+                    [X1, X2, Y] = [], [], []
+
+
+from keras.layers import *
+from keras.models import Model
+import keras.backend as K
+from keras.optimizers import Adam
+
+
+bert_model = load_trained_model_from_checkpoint(config_path, checkpoint_path)
+
+for l in bert_model.layers:
+    l.trainable = True
+
+x1_in = Input(shape=(None,))
+x2_in = Input(shape=(None,))
+
+x = bert_model([x1_in, x2_in])
+x = Lambda(lambda x: x[:, 0])(x)
+p = Dense(1, activation='sigmoid')(x)
+
+model = Model([x1_in, x2_in], p)
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=Adam(1e-5), # 用足够小的学习率
+    metrics=['accuracy']
+)
+model.summary()
+
+
+train_D = data_generator(train_data)
+valid_D = data_generator(valid_data)
+
+model.fit_generator(
+    train_D.__iter__(),
+    steps_per_epoch=len(train_D),
+    epochs=5,
+    validation_data=valid_D.__iter__(),
+    validation_steps=len(valid_D)
+)
